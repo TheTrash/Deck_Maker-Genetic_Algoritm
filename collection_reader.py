ad2dcf7b427e9192965d09fed9081f2bb75783b5
TheTrash <mattia.polticchia@gmail.com>
2021-09-22 15:51:24 +0200

    Creazione della libreria

--- collection_reader.py
+++ collection_reader.py
@@ -1,13 +1,18 @@
-import json
-from card import Card
+
 
 f = open('my_collection.json',)
 
 data = json.load(f)
 collection = []
+
 for i in range(len(data)):
     c = data[i]
     card = Card(c["attack"],c["defense"],c["ability"])
     collection.append(card)
-    print(card.to_json())
-print(len(collection))
+
+collection_len = len(collection)
+
+def makeDeck(len_deck = 10):
+    deck = rnd.sample(collection,10)
+    return deck
+
